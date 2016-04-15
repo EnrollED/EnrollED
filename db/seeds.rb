4c6bdd02e13d9897020c501d45bdc97d08a9c311
Tilen Faganel <tilen.faganel@me.com>
2016-04-15 14:21:53 +0200

    Added the default admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+user = User.new(username: 'admin', firstname: 'Admin', lastname: 'Strator', email: 'admin@example.com', password: 'supergeslo')
+user.skip_confirmation!
+user.save!
