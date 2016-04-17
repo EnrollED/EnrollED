f2a91436aa089c8d232c9be9d41687cffd845d75
Tilen Faganel <tilen.faganel@me.com>
2016-04-17 16:40:02 +0200

    Added user list menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def gravatar_url(user, size)
-    "//gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email.downcase)}.png?s=#{size}"
+  def gravatar_url(email, size = 160)
+    "//gravatar.com/avatar/#{Digest::MD5.hexdigest(email.downcase)}.png?s=#{size}&d=wavatar"
   end
 end
