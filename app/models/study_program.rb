ce628017dda432eb6cfd7e4dc1300338b06795f6
Jan Kos <jan@k0s.si>
2016-05-18 16:19:22 +0200

    Destroy study program mode on deletion of study program

--- app/models/study_program.rb
+++ app/models/study_program.rb
@@ -2,7 +2,7 @@ class StudyProgram < ActiveRecord::Base
   belongs_to :higher_education_institution
   belongs_to :type_of_study
   belongs_to :enrollment
-  has_many :study_program_modes
+  has_many :study_program_modes, :dependent => :destroy
 
   def self.search(search)
     if search
