a437125c956ee3261b11e74165740973cd6a62f9
Jan Kos <jan@k0s.si>
2016-05-18 11:18:03 +0200

    Study programs index

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:edit, :update]
+  resources :study_programs
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
