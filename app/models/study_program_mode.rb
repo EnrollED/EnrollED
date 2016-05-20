02763ec4d6449c358a8d17bbbe49a24621c06409
Jan Kos <jan@k0s.si>
2016-05-20 11:54:36 +0200

    Basic validation addded

--- app/models/study_program_mode.rb
+++ app/models/study_program_mode.rb
@@ -2,4 +2,6 @@ class StudyProgramMode < ActiveRecord::Base
   belongs_to :study_program
   belongs_to :mode_of_study
   validates :mode_of_study_id, uniqueness: {scope: [:study_program_id]}
+  validates :number_of_places, presence: true
+  validates :number_of_places_foreign, presence: true
 end
