640872db02b2c144c62e665ee2ca54cc93092036
Tilen Faganel <tilen.faganel@me.com>
2016-05-26 22:55:53 +0200

    Moved the menus to its own partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def gravatar_url(email, size = 160)
     "//gravatar.com/avatar/#{Digest::MD5.hexdigest(email.downcase)}.png?s=#{size}&d=wavatar"
   end
+
+  def nav_link_to(text, path)
+    options = current_page?(path) ? { class: 'active' } : {}
+    content_tag(:li, options) do
+      link_to text, path
+    end
+  end
 end
--- app/views/study_programs/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @study_program, :id, :created_at, :updated_at
