de609e8b1f526b4c542f7c56daba8e521d82dbef
Jan Kos <jan@k0s.si>
2016-05-26 20:40:33 +0200

    Validate presence of reqiurement element attributes

--- app/models/requirement_element.rb
+++ app/models/requirement_element.rb
@@ -1,4 +1,7 @@
 class RequirementElement < ActiveRecord::Base
   belongs_to :requirement
   belongs_to :element
+
+  validates :requirement_id, presence: true
+  validates :element_id, presence: true
 end
