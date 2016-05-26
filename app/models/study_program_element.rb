c89474c3394f18e57a47aa6f2408ffc91895a7d3
Jan Kos <jan@k0s.si>
2016-05-26 12:03:52 +0200

    Show study program element percentage

--- app/models/study_program_element.rb
+++ app/models/study_program_element.rb
@@ -4,4 +4,9 @@ class StudyProgramElement < ActiveRecord::Base
 
   validates :element, presence: true
   validates :weight, presence: true
+
+
+  def percentage
+    ( self.weight/self.requirement.study_program_elements.sum(:weight)*100 )
+  end
 end
